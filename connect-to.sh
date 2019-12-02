9f265d0990a1ba137a008f325251e14464044527
Artem <tema.ni2011@yandex.ru>
2019-12-02 18:37:32 +0300

    Shell scripts and complete symfony connection

---
+++ connect-to.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker exec -i -t --privileged "$1" bash
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+green=$(tput setf 2)
+toend=$(tput hpa $(tput cols))$(tput cub 6)
+
+docker-compose up -d --force-recreate --build
