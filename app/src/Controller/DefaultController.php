a1d5dc9f0ca3abdc9c26fc707cd04947eb97fa2d
Artem <tema.ni2011@yandex.ru>
2019-12-07 15:00:55 +0300

    Ready Vue.js app

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -3,15 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="index")
+     * @Route("/", name="index")
      */
     public function index()
     {
         return $this->render('index.html.twig');
     }
+        /**
+         * @Route("/about", name="index2")
+         */
+        public function about()
+        {
+                    return new Response(
+                        '<html><body>Gay</body></html>'
+                    );
+        }
 }
