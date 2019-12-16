30f37677756240191332125d8cb9aa5521ac1255
Artem <tema.ni2011@yandex.ru>
2019-12-16 15:59:44 +0300

    Vue Routing

--- app/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import Example from './components/Example'
-/**
- * Create a fresh Vue Application instance
- */
-new Vue({
-    el: '#app',
-    components: {Example}
-});
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -23,7 +23,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('app', './assets/vue/index.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -32,7 +32,8 @@ Encore
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+    //.enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
@@ -61,7 +62,7 @@ Encore
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+    .enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
