5eafec652ac1e3d9d92000b5d881cc6981830da4
Artem <tema.ni2011@yandex.ru>
2019-12-16 17:19:16 +0300

    Add rest bundle, downgrade symfony from v5 to v4

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
